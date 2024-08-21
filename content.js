d8ec16b925df5de0dccb585856e3a7fe2c248fc1
Konstantin Howard <kdhoward@princeton.edu>
2024-08-20 22:55:41 -0400

    more images chosen randomly

--- content.js
+++ content.js
@@ -3,17 +3,20 @@
       replaceImages(request.adUrl);
     }
   }); */
+
+  const NUM_PICS = 21;
   
   function replaceAllImages() {
     console.log("infunc");
     const images = document.querySelectorAll('img');
     console.log(images.length);
     images.forEach((img) => {
+      
       const originalWidth = img.width;
       const originalHeight = img.height;
 
-      // Set a temporary src to force the image to load
-      img.src = chrome.runtime.getURL("images/icon128.png");
+      let img_id = Math.floor(Math.random() * NUM_PICS)
+      img.src = chrome.runtime.getURL(`images/pics/${img_id}.png`);
 
       // Apply CSS to make sure the new image fits within the original dimensions
       img.style.width = `${originalWidth}px`;
@@ -22,13 +25,7 @@
       // Optionally, you might want to use object-fit to control how the image scales
       img.style.objectFit = 'contain';
     });
-    console.log("inserting!");
   }
 
-  if (!(document.documentElement instanceof HTMLElement)) {
-    return; // Only run on HTML pages
-  }
-  
-  console.log("pre load");
-  replaceAllImages();
-  console.log("post load");
+  window.addEventListener('load', replaceAllImages)
+  console.log("done")
